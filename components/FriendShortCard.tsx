6794046937cdfa03572e9395cb118c25e9f451d0
MonicaHicks <96900570+MonicaHicks@users.noreply.github.com>
2025-05-03 18:58:01 -0700

    Updating profile pic name in friend card

--- components/FriendShortCard.tsx
+++ components/FriendShortCard.tsx
@@ -2,36 +2,31 @@ import { Image, View } from "react-native";
 import theme from "../assets/theme";
 import { ThemedText } from "./ThemedText";
 import { ThemedView } from "./ThemedView";
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function FriendShortCard({
-    friendInfo
+  friendInfo,
 }: {
-    // Not fully sold on this freinds schema yet.
-    friendInfo: {
-        id: string;
-        name: string;
-        image: string;
-        eventsAttending: string[];
-    };
+  // Not fully sold on this freinds schema yet.
+  friendInfo: {
+    id: string;
+    name: string;
+    image: string;
+    eventsAttending: string[];
+  };
 }) {
-    return (
-        <ThemedView style={theme.friendCard}>
-            <View style={theme.profilePicNameContainer}>
-                <Image
-                    source={require("../assets/images/placeholderClub.png")}
-                    style={theme.profilePic}
-                />
-                <View style={theme.profileAndDisplayName}>
-                <ThemedText type="subtitle">
-                    {friendInfo.name}
-                </ThemedText>
-                <ThemedText type="default">
-                    displayname
-                </ThemedText>
-            </View>
-            </View>
-        </ThemedView>
-    );
-    // List the soonest 3 events for each friend
-};
+  return (
+    <ThemedView style={theme.friendCard}>
+      <View style={theme.profilePicNameContainer}>
+        <Image
+          source={require("../assets/images/Placeholder_Club.png")}
+          style={theme.profilePic}
+        />
+        <View style={theme.profileAndDisplayName}>
+          <ThemedText type="subtitle">{friendInfo.name}</ThemedText>
+          <ThemedText type="default">displayname</ThemedText>
+        </View>
+      </View>
+    </ThemedView>
+  );
+  // List the soonest 3 events for each friend
+}
